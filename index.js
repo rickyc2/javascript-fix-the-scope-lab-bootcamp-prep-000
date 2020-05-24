57e719544724550b88f8e821d7bf9ae2f09952fc
darling-thread-6760 <mistawilliams210@gmail.com>
2020-05-24 19:17:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var animal = 'dog'
 
 function myAnimal() {
   // You should not need to modify this function
-  return animal
+  return function yourAnimal() { }
+  return animal 
 }
 
 function yourAnimal() {
