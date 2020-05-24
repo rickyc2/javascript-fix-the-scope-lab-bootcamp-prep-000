3f7e343e99c9a9fe47242869aec877661beba814
darling-thread-6760 <mistawilliams210@gmail.com>
2020-05-24 19:16:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  return function myAnimal()
+  return function myAnimal() {
+    var 
+  }
   return animal 
 }
 
