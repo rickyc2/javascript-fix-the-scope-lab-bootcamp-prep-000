e6776e3a433f5fbf573271c2c9bc327765429447
darling-thread-6760 <mistawilliams210@gmail.com>
2020-05-24 19:29:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+   const two = 2
+  
 
   // Feel free to move things around!
-  const two = 2
+ return n + two
 }
 
 var funkyFunction = function() {
